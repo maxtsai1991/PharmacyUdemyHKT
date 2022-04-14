20020d47dee095d671dde66237b42f954e4d1f3d
MAX-DELL-5410\s8604 <maxtsai@esobi.com>
2022-04-14 14:34:43 +0800

    3-10 OKHttp 基本使用套路介紹 (第三節:網路連線程式 , Udemy_HKT老師)

--- app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
+++ app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
@@ -3,6 +3,15 @@ package com.example.pharmacyudemyhkt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 3-10 基本使用套件介紹
+ * (OkHttp 獲取網路資料方式參考網址 : https://tw-hkt.blogspot.com/2020/12/android-okhttp.html)
+ * 補充說明 :
+ * GET :  直接在瀏覽器,輸入網址,直接執行,瀏覽器就會跑出資料,這種請求方式大部分就是GET方式
+ * POST : 帳號密碼登入,透過表單去傳遞資料,這種請求方式就是POST方式
+ * Execute Synchronous(執行同步) :整個系統,會等你把整個資料抓回來,我們在去執行之後的動作
+ * Enqueue Synchronous(排隊非同步) :送出了這個請求之後,剛剛所要抓資料這部分,幫你在背後做執行,而不會阻塞整個主線程,大部分都會使用非同步,因為Android會限制你,拉網路資料(很耗時的),不允許把整個畫面卡死
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
