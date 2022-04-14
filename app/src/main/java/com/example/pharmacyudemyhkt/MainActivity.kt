b633e1d8520d2feff4fadd2a6a7c31e8ba9a6579
MAX-DELL-5410\s8604 <maxtsai@esobi.com>
2022-04-14 11:32:10 +0800

    First git commit test

--- app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
+++ app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.pharmacyudemyhkt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// git commit test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
