e0b6545ad1fca4bc5f645daf170977f9b28ba8a9
MAX-DELL-5410\s8604 <maxtsai@esobi.com>
2022-04-14 11:32:51 +0800

    git commit test2

--- app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
+++ app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.pharmacyudemyhkt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// git commit test
+// git commit test2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
