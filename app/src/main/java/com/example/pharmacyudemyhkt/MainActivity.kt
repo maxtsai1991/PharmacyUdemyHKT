7a09bc802516c531f7404112df202cc8c6198f4b
MAX-DELL-5410\s8604 <maxtsai@esobi.com>
2022-04-14 14:59:07 +0800

    3-11 實戰OKHttp中的Enqueue非同步處理GET請求方式 (第三節:網路連線程式 , Udemy_HKT老師)

--- app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
+++ app/src/main/java/com/example/pharmacyudemyhkt/MainActivity.kt
@@ -2,6 +2,15 @@ package com.example.pharmacyudemyhkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
+
+/**
+ * 3-8 & 3-9
+ * 1.   添加 OkHttp 依賴庫 , 在 GRADLE (Module) 層級的 dependencies 中內加入 EX : implementation 'com.squareup.okhttp3:okhttp:4.9.0'
+ * 2.   權限宣告 (permissions) , 連線網路，需在 AndroidManifest.xml 中宣告 EX : <uses-permission android:name="android.permission.INTERNET" />
+ */
 
 /**
  * 3-10 基本使用套件介紹
@@ -13,10 +22,48 @@ import android.os.Bundle
  * Enqueue Synchronous(排隊非同步) :送出了這個請求之後,剛剛所要抓資料這部分,幫你在背後做執行,而不會阻塞整個主線程,大部分都會使用非同步,因為Android會限制你,拉網路資料(很耗時的),不允許把整個畫面卡死
  */
 
+/**
+ * 3-11 實戰OKHttp中的Enqueue非同步處理GET請求方式
+ * 口罩資料網址 : https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json
+ */
+
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val TAG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getPharmacyData()
     }
+
+    private fun getPharmacyData() {
+
+        //口罩資料網址 (資料來源)
+        val pharmaciesDataUrl = "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
+
+        //Part 1: 宣告(設定)OkHttpClient
+        val okHttpClient : OkHttpClient = OkHttpClient().newBuilder().build()
+
+        //Part 2: 宣告(設定)Request，要求要連到指定網址 ,透過GET方式
+        val request : Request = Request.Builder().url(pharmaciesDataUrl).get().build()
+
+        //Part 3: 宣告 Call ,要連線到哪個網址,用怎麼樣方式去做連線,把上面的request塞給newCall
+        val call : Call = okHttpClient.newCall(request)
+
+        //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料 , 實作兩個覆寫方法(失敗&成功)
+        call.enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d(TAG,"onFailure: $e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d(TAG, "onResponse: ${response.body?.string()}") // body有可能為空null,所以要加?(問號)
+            }
+        })
+
+    }
+
 }
