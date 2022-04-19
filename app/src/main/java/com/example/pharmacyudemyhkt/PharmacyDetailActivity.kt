968e5e51a4866f8d128a6ab21f0d4dbacb6f1d76
MAX-DELL-5410\s8604 <maxtsai@esobi.com>
2022-04-19 20:55:14 +0800

    4-42 Intent基本跳頁和資料傳遞 (第四節:使用者介面元件-RecyclerView , Udemy_HKT老師)

--- app/src/main/java/com/example/pharmacyudemyhkt/PharmacyDetailActivity.kt
+++ app/src/main/java/com/example/pharmacyudemyhkt/PharmacyDetailActivity.kt
@@ -2,12 +2,31 @@ package com.example.pharmacyudemyhkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.pharmacyudemyhkt.data.Feature
 
 class PharmacyDetailActivity : AppCompatActivity() {
+    val TAG = PharmacyDetailActivity::class.java.simpleName
+
+    /**
+     * 萬惡錯誤 NullPointerException (參考 : https://tw-hkt.blogspot.com/2021/01/recyclerview.html)
+     *      假設資料，沒有正確傳遞過來，將會引起 Caused by: java.lang.NullPointerException 造成 APP 不可預期閃退，所以需要再修改一下，確保資料為空時，仍可以正常顯示。
+     */
+
+    /**
+     * 獲取data
+     * 其中 「as Feature」，是指強行將資料轉換成 Feature。「as? Feature」，as 後面多一個問號，是確保資料不為空才進行轉換。
+     */
+    private val data by lazy {
+        intent.getSerializableExtra("data") as? Feature
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pharmacy_detail)
 
+        //印出資料
+        Log.d(TAG,"PharmacyDetailActivity_info: "+ data?.property?.name);
     }
 
 }
